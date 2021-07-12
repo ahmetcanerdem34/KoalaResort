56d593aa345d0b6a30eab5f345257bb4df3c6537
test <81445856+fafataris@users.noreply.github.com>
2021-07-12 15:56:56 +0300

    negatif oda olusturma

--- src/test/java/com/techproed/pages/LoginPage.java
+++ src/test/java/com/techproed/pages/LoginPage.java
@@ -4,6 +4,7 @@ import com.techproed.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
 
 public class LoginPage {
     public LoginPage(){
@@ -21,5 +22,6 @@ public class LoginPage {
 
     @FindBy(id="divMessageResult")
     public WebElement errorMessage;
+
 
 }
